13d1a5b38b46e4189d4ec540ad743729647dad84
kwangsooshin <kepler92@daum.net>
2019-06-03 19:47:53 +0900

    .

--- mapper.py
+++ mapper.py
@@ -2,5 +2,5 @@ import sys
 
 for index, line in enumerate(sys.stdin):
     if index != 0:
-        group, number = line.split(",")
+        group, number = line.strip().split(",")
         print("{},{}".format(group, number))
