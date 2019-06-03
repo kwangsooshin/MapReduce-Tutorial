8129c6f77dbae0582c7fbc5b81614f7b6288bf0e
kwangsooshin <kepler92@daum.net>
2019-06-03 19:30:58 +0900

    .

--- plain.py
+++ plain.py
@@ -1,16 +1,16 @@
-with open("input.data", "r") as f:
-    number_of_group = int(f.readline())
+import sys
 
-    counter = [0 for x in range(number_of_group)]
+number_of_group = int(sys.stdin)
+counter = [0 for x in range(number_of_group)]
 
-    for line in f:
-        group, number = line.split(",")
-        group = int(group)
-        number = float(number)
+for line in sys.stdin:
+    group, number = line.split(",")
+    group = int(group)
+    number = float(number)
 
-        if number >= counter[group]:
-            counter[group] = number
+    if number >= counter[group]:
+        counter[group] = number
 
-    for i in range(number_of_group):
-        print("{},{}".format(i, counter[i]))
+for i in range(number_of_group):
+    print("{},{}".format(i, counter[i]))
 
